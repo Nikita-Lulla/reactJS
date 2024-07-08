9f41398b2269b86f72cedb4a3b8bd500a17f2706
Nikita <nikitalulla9284@gmail.com>
2024-07-08 14:26:57 +0530

    Syncing the code

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// function App() {
+//   return (
+//     <>
+//     <h1>Good Afternoon!</h1>
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank" 
+//           rel="noopener noreferrer"
+//         >
+//           Let's learn React
+//         </a>
+//       </header>
+//     </div>
+//     </>
+//   );
+// }
+// let name = "Nikita";
+
+function App(){
+  return(
+    <>
+<Navbar />
+    </>
+    
   );
 }
-
 export default App;
